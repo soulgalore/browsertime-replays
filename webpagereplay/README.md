d8ca3f6506af7f50712586911d145df94b0e66e6
Peter Hedenskog <phedenskog@wikimedia.org>
2017-12-07 13:20:15 +0100

    support FF and Chrome

--- webpagereplay/README.md
+++ webpagereplay/README.md
@@ -1,8 +1,6 @@
 # Browsertime + WebPageReplay
 Use Browsertime and WebPageReplay together.
 
-The current version only works for Chrome. To get it to work for Firefox we need to install the CA and have host-resolver-rules for Firefox.
-
 It works like this:
 1. The start script starts WebPageReplay in record mode
 2. Then starts Browsertime accessing the URL you choose one time (so it is recorded)
@@ -18,8 +16,16 @@ docker build -t soulgalore/browsertime-webpagereplay .
 ```
 
 ## Run
-You can change the latency and number of runs with LATENCY and RUNS.
+You can change browser, latency and number of runs with BROWSER, LATENCY and RUNS.
+
+Default browser is Chrome:
 
 ```
 docker run --cap-add=NET_ADMIN --shm-size=1g --rm -v "$(pwd)":/browsertime -e RUNS=11 -e LATENCY=100 soulgalore/browsertime-webpagereplay https://en.wikipedia.org/wiki/Barack_Obama
+```
+
+Use Firefox (note that we need to skipHar until there's a new Firefox plugin for 57):
+
+```
+docker run --cap-add=NET_ADMIN --shm-size=1g --rm -v "$(pwd)":/browsertime -e BROWSER=firefox -e RUNS=11 -e LATENCY=100 soulgalore/browsertime-webpagereplay https://en.wikipedia.org/wiki/Barack_Obama --skipHar --firefox.acceptInsecureCerts
 ```
--- webpagereplay/start.sh
+++ webpagereplay/start.sh
@@ -4,17 +4,33 @@ set -e
 google-chrome --version
 firefox --version
 
+
 RUNS="${RUNS:-5}"
 LATENCY=${LATENCY:-100}
+BROWSER=${BROWSER:-'chrome'}
 
-webpagereplaywrapper record --start --path /root/go/src/github.com/catapult-project/catapult/web_page_replay_go
+CHROME_EXTRAS='--chrome.args host-resolver-rules="MAP *:80 127.0.0.1:80,MAP *:443 127.0.0.1:443,EXCLUDE localhost"'
+FIREFOX_EXTRAS='--firefox.preference "network.dns.forceResolve:127.0.0.1"'
 
-/usr/src/app/bin/browsertime.js --chrome.args host-resolver-rules="MAP *:80 127.0.0.1:8080,MAP *:443 127.0.0.1:8081,EXCLUDE localhost" -n 1 --pageCompleteCheck "return true;" "$@"
+EXTRAS=''
 
-webpagereplaywrapper record --stop --path /root/go/src/github.com/catapult-project/catapult/web_page_replay_go
+if [ $BROWSER = 'chrome' ]
+then
+  EXTRAS=$CHROME_EXTRAS
+else
+  EXTRAS=$FIREFOX_EXTRAS
+fi
 
-webpagereplaywrapper replay --start --path /root/go/src/github.com/catapult-project/catapult/web_page_replay_go
+webpagereplaywrapper record --start --path /root/go/src/github.com/catapult-project/catapult/web_page_replay_go --http 80 --https 443
 
-/usr/src/app/bin/browsertime.js --chrome.args host-resolver-rules="MAP *:80 127.0.0.1:8080,MAP *:443 127.0.0.1:8081,EXCLUDE localhost" -n $RUNS --video --speedIndex --pageCompleteCheck "return true;" --connectivity.engine throttle --connectivity.throttle.localhost --connectivity.profile custom --connectivity.latency $LATENCY "$@"
 
-webpagereplaywrapper replay --stop --path /root/go/src/github.com/catapult-project/catapult/web_page_replay_go
+/usr/src/app/bin/browsertime.js -b $BROWSER -n 1 "$EXTRAS" --pageCompleteCheck "return true;" "$@"
+
+webpagereplaywrapper record --stop --path /root/go/src/github.com/catapult-project/catapult/web_page_replay_go --http 80 --https 443
+
+webpagereplaywrapper replay --start --path /root/go/src/github.com/catapult-project/catapult/web_page_replay_go --http 80 --https 443
+
+
+/usr/src/app/bin/browsertime.js -b $BROWSER -n $RUNS "$EXTRAS" --video --speedIndex --pageCompleteCheck "return true;" --connectivity.engine throttle --connectivity.throttle.localhost --connectivity.profile custom --connectivity.latency $LATENCY "$@"
+
+webpagereplaywrapper replay --stop --path /root/go/src/github.com/catapult-project/catapult/web_page_replay_go --http 80 --https 443
